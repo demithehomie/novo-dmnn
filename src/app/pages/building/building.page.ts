1b2645a1c47c94b362bf95af6ad29709ecbd6f45
tripleAVB <venturebuildertriplea@gmail.com>
2023-04-07 23:35:18 -0300

    segundo deploy a

--- src/app/pages/building/building.page.ts
+++ src/app/pages/building/building.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-building',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuildingPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
+
+  copyLink(){
+
+    
+}
+  openExternalLinkWhatsAppLuan(){
+    window.open('https://wa.me/5522988542742?text=Ol%C3%A1%21+Vim+da+Plataforma+DMNS.+Preciso+de+algumas+informa%C3%A7%C3%B5es.+Pode+me+ajudar%3F', '_blank')
+  }
+
+
+  openExternalLinkWhatsAppDemi(){
+    window.open('https://wa.me/5521969648455?text=Ol%C3%A1%21+Vim+da+Plataforma+DMNS.+Preciso+de+algumas+informa%C3%A7%C3%B5es.+Pode+me+ajudar%3F', '_blank')
+  }
+  
 
 }
